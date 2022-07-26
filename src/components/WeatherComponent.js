56b8709707fa4e531b23e44481e6d03ccdd2217a
Akshayteli-111 <93920968+Akshayteli-111@users.noreply.github.com>
2022-07-26 15:34:19 +0530

    Update WeatherComponent.js

--- src/components/WeatherComponent.js
+++ src/components/WeatherComponent.js
@@ -1,9 +1,9 @@
 import React from "react";
+import WeatherInfoComponent from "./WeatherInfoComponent";
 import "../App.css";
- import WeatherInfoComponent from "./WeatherInfoComponent";
 
 const WeatherIcons = {
-   "01d": "/icons/sunny.svg",
+  "01d": "/icons/sunny.svg",
   "01n": "/icons/night.svg",
   "02d": "/icons/day.svg",
   "02n": "/icons/cloudy-night.svg",
@@ -20,32 +20,45 @@ const WeatherIcons = {
 };
 
 const WeatherComponent = (props) => {
-    const {weather} = props;
-    const isDay = weather?.weather[0].icon?.includes('d')
-    const getTime = (timeStamp) => {
-        return `${new Date(timeStamp * 1000).getHours()} : ${new Date(timeStamp * 1000).getMinutes()}`
-    }
-    return (
-        <>
-            <WeatherContainer>
-                <Condition>
-                    <span>{`${Math.floor(weather?.main?.temp - 273)}°C`}</span>
-                    {`  |  ${weather?.weather[0].description}`}
-                </Condition>
-                <WeatherIcon src={WeatherIcons[weather?.weather[0].icon]}/>
-            </WeatherContainer>
-            <Location>{`${weather?.name}, ${weather?.sys?.country}`}</Location>
+  const { weather } = props;
+  const isDay = weather?.weather[0].icon?.includes("d");
+  const getTime = (timeStamp) => {
+    return `${new Date(timeStamp * 1000).getHours()} : ${new Date(
+      timeStamp * 1000
+    ).getMinutes()}`;
+  };
+  return (
+    <>
+      <div className="WeatherContainer">
+        <span className="Condition">
+          <span>{`${Math.floor(weather?.main?.temp - 273)}°C`}</span>
+          {`  |  ${weather?.weather[0].description}`}
+        </span>
+        <img
+          className="WeatherIcon"
+          src={WeatherIcons[weather?.weather[0].icon]}
+        />
+      </div>
+      <span className="Location">{`${weather?.name}, ${weather?.sys?.country}`}</span>
 
-            <WeatherInfoLabel>Weather Info</WeatherInfoLabel>
-            <WeatherInfoContainer>
-                <WeatherInfoComponent name={isDay ? "sunset" : "sunrise"}
-                                      value={`${getTime(weather?.sys[isDay ? "sunset" : "sunrise"])}`}/>
-                <WeatherInfoComponent name={"humidity"} value={weather?.main?.humidity}/>
-                <WeatherInfoComponent name={"wind"} value={weather?.wind?.speed}/>
-                <WeatherInfoComponent name={"pressure"} value={weather?.main?.pressure}/>
-            </WeatherInfoContainer>
-        </>
-    );
+      <span className="WeatherInfoLabel">Weather Info</span>
+      <div className="WeatherInfoContainer">
+        <WeatherInfoComponent
+          name={isDay ? "sunset" : "sunrise"}
+          value={`${getTime(weather?.sys[isDay ? "sunset" : "sunrise"])}`}
+        />
+        <WeatherInfoComponent
+          name={"humidity"}
+          value={weather?.main?.humidity}
+        />
+        <WeatherInfoComponent name={"wind"} value={weather?.wind?.speed} />
+        <WeatherInfoComponent
+          name={"pressure"}
+          value={weather?.main?.pressure}
+        />
+      </div>
+    </>
+  );
 };
 
 export default WeatherComponent;
