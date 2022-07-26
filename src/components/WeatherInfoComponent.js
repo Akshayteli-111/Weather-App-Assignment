c0a04a82fd959661ac1b51705b62e2576d1a036e
Akshayteli-111 <93920968+Akshayteli-111@users.noreply.github.com>
2022-07-26 15:35:06 +0530

    Update WeatherInfoComponent.js

--- src/components/WeatherInfoComponent.js
+++ src/components/WeatherInfoComponent.js
@@ -1,26 +1,24 @@
 import React from "react";
 import "../App.css";
 
-
-
 export const WeatherInfoIcons = {
-    sunset: "/icons/temp.svg",
-    sunrise: "/icons/temp.svg",
-    humidity: "/icons/humidity.svg",
-    wind: "/icons/wind.svg",
-    pressure: "/icons/pressure.svg",
+  sunset: "/icons/temp.svg",
+  sunrise: "/icons/temp.svg",
+  humidity: "/icons/humidity.svg",
+  wind: "/icons/wind.svg",
+  pressure: "/icons/pressure.svg",
 };
 
 const WeatherInfoComponent = (props) => {
-    const {name, value} = props;
-    return (
-        <InfoContainer>
-            <InfoIcon src={WeatherInfoIcons[name]}/>
-            <InfoLabel>
-                {value}
-                <span>{name}</span>
-            </InfoLabel>
-        </InfoContainer>
-    );
+  const { name, value } = props;
+  return (
+    <div className="InfoContainer">
+      <img className="InfoIcon" src={WeatherInfoIcons[name]} />
+      <span className="InfoLabel">
+        {value}
+        <span>{name}</span>
+      </span>
+    </div>
+  );
 };
 export default WeatherInfoComponent;
